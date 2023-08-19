881b0f149b2d3489aeb440c64cb4a75961fe96b9
Марина <mika-klubnika@yandex.ru>
2023-08-19 15:48:17 +0300

    добавляет картинки и шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -124,7 +124,7 @@ const clean = () => {
 const syncServer = () => {
   server.init({
     server: 'build/',
-    index: 'sitemap.html',
+    index: 'index.html',
     notify: false,
     open: true,
     cors: true,
